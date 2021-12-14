9c561b45b8cb84972ca6018353a24ce59d9f2458
francis-siva <fsivasithamparam@gmail.com>
2021-12-14 17:09:50 +0100

    Handling button oncClick() & input onChange

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [newtask, set_newtask] = useState('');
+    
+    const add_task = (new_task) => {
+        set_newtask(new_task);
+    }
+    const [todolist, set_todolist] = useState([
+        { name: "faire les courses", done: false },
+        { name: "faire execices react", done: false }
+    ]);
     return (
         <div className="App">
             <header className="App-header">
@@ -9,6 +19,36 @@ function App() {
             <section>
                 <div className='task'>mon message</div>
                 <div className='task done'>mon message</div>
+                <div className='more_task'>
+                    <input type="text" name="new_task"
+                        placeholder="saisir une nouvelle tâche"
+                        value={newtask}
+                        onChange={(e) => {
+                                console.log(e.target.value);
+                                set_newtask(e.target.value);
+                                
+                                console.warn("newtask-> " + newtask);                            
+
+                            }
+                        }
+
+                    />
+                    <button
+                        onClick={() => {
+                            console.warn("Click works");
+                            
+                            const todolistCopy = [...todolist];
+                            
+                            console.warn(todolistCopy);
+                            todolistCopy.push(newtask);
+                            console.warn(todolistCopy);
+                            console.log(todolistCopy.length);
+
+                            set_todolist(todolistCopy)
+                        }}
+                    >ADD TO LIST</button>
+                </div>
+                
             </section>
         </div>
     );
